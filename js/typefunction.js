d0edb3a1e8ba53983c839dd7171271877026b729
김지이 <ooouuu1@hanmail.net>
2022-10-11 17:39:47 +0900

    수정업로드

--- js/typefunction.js
+++ js/typefunction.js
@@ -1,28 +1,28 @@
-// new TypeIt(".intro-txtbig", {
-// strings: ["반갑습니다:)","웹퍼블리셔 김지이입니다"],
-// speed: 100,
-// }).go();
-
-
-
 new TypeIt(".typing", {
-  speed: 50,
-  waitUntilVisible: true,
-  loop: false
-})
+strings: ["반갑습니다:)","<strong>웹퍼블리셔 김지이</strong>입니다"],
+speed: 100,
+}).go();
 
-new TypeIt(".typing", {
-  speed: 50,
-  waitUntilVisible: true,
-  loop: false
-})
-.type("반갑습니다:)")
-.break({ delay: 500 })
-.type("<strong>웹퍼블리셔김지이<strong>")
-.pause(100) 
-.type("입니다")
-.pause(100) 
-.go(); 
+
+
+// new TypeIt(".typing", {
+//   speed: 50,
+//   waitUntilVisible: true,
+//   loop: false
+// })
+
+// new TypeIt(".typing", {
+//   speed: 50,
+//   waitUntilVisible: true,
+//   loop: false
+// })
+// .type("반갑습니다:)")
+// .break({ delay: 500 })
+// .type("<strong>웹퍼블리셔김지이<strong>")
+// .pause(100) 
+// .type("입니다")
+// .pause(100) 
+// .go(); 
 
 
 
